85ca0849d1b102e51fd7bcacc3957bdceda5b152
kowsik-22 <kowsik22.a@gmail.com>
2025-06-07 17:28:30 +0530

    initial commit

---
+++ client/cookbook-bot/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+//import './App.css'
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="p-8 bg-yellow-100 text-red-600 text-2xl font-bold rounded-lg">
+      Tailwind is working!
+    </div>
+    </>
+  )
+}
+
+export default App
